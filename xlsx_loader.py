227e53f11f7f6452fa015285fc3abec5a94ad418
ikeda042 <ikeda042main@gmail.com>
2024-06-27 20:50:49 +0900

    xlsx

--- xlsx_loader.py
+++ xlsx_loader.py
@@ -1,9 +1,30 @@
-from openpyxl import load_workbook
+from openpyxl import Workbook, load_workbook
 
-workbook = load_workbook(filename="template.xlsx")
+
+def index_to_cell(index: int) -> str:
+    chars: list[str] = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+    ]
+    return chars[index % 8] + str(index // 8 + 1)
+
+
+workbook: Workbook = load_workbook(filename="template.xlsx")
 worksheet = workbook.active
-
 
 header, *rows = [row for row in worksheet.iter_rows(values_only=True)]
 
-cols: dict[str, str] = {}
+data: list[list[str | None]] = [[j for j in i[1:]] for i in rows]
+data: list[str | None] = [item for sublist in data for item in sublist]
+
+pick_up_from: list[str] = [
+    index_to_cell(i) for i in range(len(data)) if data[i] is not None
+]
+
+print(pick_up_from)
