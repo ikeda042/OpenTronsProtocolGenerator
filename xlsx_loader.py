3ae6103dc93cbca0af1c0041cdd50e7daa0774b4
ikeda042 <ikeda042main@gmail.com>
2024-06-27 20:17:49 +0900

    xlsx

--- xlsx_loader.py
+++ xlsx_loader.py
@@ -1,1 +1,8 @@
 import pandas as pd
+import numpy as np
+
+df = pd.read_excel("template.xlsx")
+
+# iterate over the rows of the dataframe
+for index, row in df.iterrows():
+    print(row["Column1"], row["Column2"])
