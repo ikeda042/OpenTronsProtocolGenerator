730db46fa0c00d7ff302a99604ae5b73b0232d5a
ikeda042 <ikeda042main@gmail.com>
2024-06-28 15:33:38 +0900

    feat: CORS settings

--- backend/main.py
+++ backend/main.py
@@ -4,9 +4,26 @@ from fastapi.responses import FileResponse
 from pydantic import BaseModel
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
 
 app = FastAPI()
 router_dev = APIRouter()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @router_dev.get("/xlsx_template")
